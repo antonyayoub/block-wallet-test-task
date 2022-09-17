237a3cd7cd41c68657190b7175f3f42ef3205754
Antony Ayoub <aayoub@lyrise.ai>
2022-09-17 20:26:28 +0200

    Mobile Responsivity

--- src/pages/SendSuccess.tsx
+++ src/pages/SendSuccess.tsx
@@ -6,7 +6,7 @@ import defaults from '../utils/constants';
 const SendSuccess = () => {
   return (
       <div className="bg-backgroundMain w-screen h-screen py-10">
-        <div className="bg-backgroundSecondary w-3/12 h-5/6 mx-auto rounded shadow-lg py-6">
+        <div className="bg-backgroundSecondary w-10/12 h-5/6 mx-auto rounded shadow-lg py-6 lg:w-3/12">
         <div className="flex flex-col h-full w-full">
           
           <div className="h-2/3 flex flex-col justify-center items-center space-y-2">
